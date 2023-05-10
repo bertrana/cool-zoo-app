afcb41c92f10109b10e08840f8ee2840c2b95842
bertrana <botediom@gmail.com>
2023-05-10 15:13:24 +0600

    feat: props to UserCard

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -27,7 +27,8 @@ export default {
     <h2 class="testimonials__header">Testimonials</h2>
     <div class="slider">
       <ul class="slider__list">
-        <UserCard v-for="user in this.getUsersList" :key="user.id" />
+        <UserCard v-for="user in this.getUsersList" :key="user.id" :img-src="user.picture.thumbnail"
+          :user-name="user.name.first + ' ' + user.name.last" card-text="Lorem ipsum sin dolor amet" />
       </ul>
       <input type="range" name="" id="" min="1" max="8" value="2" />
     </div>
